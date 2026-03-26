63b332466a11e255c72c7e861d683340f25fcb4d
KimSeungHan <rlatmdgks4@khu.ac.kr>
2026-03-26 09:03:54 +0900

    Update String.h

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector> //Container
 #include "DynamicArray.h"
+#include "String.h"
 
 using namespace std;
 
@@ -36,13 +37,32 @@ public:
 
 int main()
 {
-	FVector2D First(0, 0);
-	FVector2D Second(2, 2);
+	//FVector2D First(0, 0);
+	//FVector2D Second(2, 2);
 
-	FVector2D Three(First + Second);
+	//FVector2D Three(First + Second);
 
-	Three = First + Second;
+	//Three = First + Second;
 
-	First + Second;
-	// = First.operator+(Second); 위에 거랑 같음
+	//First + Second;
+	//// = First.operator+(Second); 위에 거랑 같음
+
+	char p[5] = "1234";
+	String s(p);
+	String s1 = "987";
+	s = "4234";
+	cout << s.length() << endl;
+	cout << s[0] << endl;
+	s.pushback("56");
+	cout << s[5] << endl;
+	cout << s.length() << endl;
+	s = s1;
+	cout << s.length() << endl;// <- 오류남
+	s = s + s1;
+	cout << s.length() << endl;
+	//cout << s.c_str()//Char*
+
+
+	return 0;
+
 }
